11296af337f05ae304985197fdedeecdb02a0b57
sanglb <sang.le.ba@sobanhang.com>
2024-01-23 01:08:00 +0700

    update

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -20,7 +20,7 @@ const Login = ({ heading, role }) => {
       // if (res.payload.data.roleId === 0) history.push("/admin");
       // else history.goBack();
       // <Link to={`${path.admin}`}>Dashboard</Link>
-      if (res.payload.data.user.roleId === (2||3))  {
+      if (res.payload.data.user.roleId === 2||3)  {
         localStorage.setItem( "admin",  true  );
         history.push("/admin");
     } else {
